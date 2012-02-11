e117b52759a4cb6cba3064db208fe0f52dd5d3df
Arto O Vihavainen <avihavai@e42-5.cs.helsinki.fi>
2012-02-11 18:03:19 +0200

    poistettu viitteet olemattomaan projectserviceen

--- tokkel-hw/src/main/java/wad/tokkel/services/TaskServiceImpl.java
+++ tokkel-hw/src/main/java/wad/tokkel/services/TaskServiceImpl.java
@@ -6,43 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import wad.tokkel.models.Project;
 import wad.tokkel.models.Task;
-import wad.tokkel.repositories.ProjectRepository;
 import wad.tokkel.repositories.TaskRepository;
 
 @Service("taskService")
 public class TaskServiceImpl extends AbstractRepositoryService<Task>
-implements TaskService {
-  @Autowired
-  private TaskRepository taskRepository;
+        implements TaskService {
 
-  @Autowired
-  private ProjectRepository projectRepository;
+    @Autowired
+    private TaskRepository taskRepository;
 
-  @PostConstruct
-  private void init() {
-    setRepository(taskRepository);
-  }
-
-  @Override
-  @Transactional(propagation = Propagation.REQUIRED, readOnly = true,
-          rollbackFor = Throwable.class)
-  public List<Task> findByProject(Integer projectId) {
-    return taskRepository.findByProject(projectId);
-  }
-
-  @Override
-  @Transactional(propagation = Propagation.REQUIRED, readOnly = false,
-          rollbackFor = Throwable.class)
-  public Task save(Task task, Integer projectId) {
-    Project project = null;
-    if (projectId != null) {
-      project = projectRepository.findOne(projectId);
-      project.getTasks().add(task);
+    // LISÄÄ TÄNNE PROJECTREPOSITORY
+    @PostConstruct
+    private void init() {
+        setRepository(taskRepository);
     }
-    task.setProject(project);
-    taskRepository.saveAndFlush(task);
-    return task;
-  }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = true,
+    rollbackFor = Throwable.class)
+    public List<Task> findByProject(Integer projectId) {
+        return taskRepository.findByProject(projectId);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false,
+    rollbackFor = Throwable.class)
+    public Task save(Task task, Integer projectId) {
+        // HAE TÄSSÄ PROJEKTI JA TALLENNA SIIHEN LIITTYVÄ TASKI
+
+        taskRepository.saveAndFlush(task);
+        return task;
+    }
 }
